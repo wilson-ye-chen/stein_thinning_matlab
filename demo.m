a4e950987bf4db62119080b962278fd986345c73
Wilson Ye Chen <ye.chen@uts.edu.au>
2020-05-20 22:55:39 +1000

    Renamed test.m as demo.m

--- demo.m
+++ demo.m
@@ -3,13 +3,13 @@ X = csvread('smp.csv');
 G = csvread('scr.csv');
 
 m = 40;
-[pi, ksd] = thin(X, G, m);
+[idx, ksd] = thin(X, G, m);
 
 % Plot point-set over trace
 plot(X(:, 1), X(:, 2), '-')
 hold on;
-plot(X(pi, 1), X(pi, 2), '.', 'markersize', 30)
+plot(X(idx, 1), X(idx, 2), '.', 'markersize', 30)
 
-% Plot KSD
+% Plot KSD path
 figure()
 semilogy(1:m, ksd)
